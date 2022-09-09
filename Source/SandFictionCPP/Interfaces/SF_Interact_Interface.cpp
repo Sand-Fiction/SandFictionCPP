b175f6eac1ea4c2fdc6a3d618849b3d23f5bc776
Moritz Peter <momomenion@gmail.com>
2022-09-09 12:29:01 +0200

    Base Modules

---
+++ Source/SandFictionCPP/Interfaces/SF_Interact_Interface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SF_Interact_Interface.h"
+
+// Add default functionality here for any ISF_Interact_Interface functions that are not pure virtual.
---
+++ Source/SandFictionCPP/SF_CameraManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/PlayerCameraManager.h"
+#include "SF_CameraManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SANDFICTIONCPP_API ASF_CameraManager : public APlayerCameraManager
+{
+	GENERATED_BODY()
+	
+};

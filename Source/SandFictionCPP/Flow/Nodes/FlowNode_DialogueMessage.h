1a6eb8a3f3df0d7059b3f505cb6622f17f6d3a6e
Moritz Peter <momomenion@gmail.com>
2023-06-03 16:39:23 +0200

    Dialogue Structs for Custom Voice / Anims per DialogueLine

--- Source/SandFictionCPP/Flow/Nodes/FlowNode_DialogueMessage.h
+++ Source/SandFictionCPP/Flow/Nodes/FlowNode_DialogueMessage.h
@@ -6,9 +6,36 @@
 #include "Engine/DataTable.h"
 #include "Nodes/FlowNode.h"
 #include "FlowNode_DialogueMessage.generated.h"
-/**
- * 
- */
+
+USTRUCT(BlueprintType)
+struct FDialogueLineStruct
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	FText DialogueLine;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	UAnimMontage* AnimMontage;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	USoundBase* Voice;
+
+	FDialogueLineStruct()
+	{
+		DialogueLine = FText();
+		AnimMontage = nullptr;
+		Voice = nullptr;
+	}
+
+	FDialogueLineStruct(const FText Dialogue)
+	{
+		DialogueLine = Dialogue;
+		AnimMontage = nullptr;
+		Voice = nullptr;
+	}
+};
+
 UCLASS(NotBlueprintable, meta = (DisplayName = "Dialogue Message"))
 class SANDFICTIONCPP_API UFlowNode_DialogueMessage final : public UFlowNode
 {
@@ -21,6 +48,12 @@ public:
 
 	UPROPERTY(EditAnywhere)
 	TArray<FText> DialogueLines;
+
+	UPROPERTY(EditAnywhere)
+	TArray<FDialogueLineStruct> NewDialogueLines;
+
+	UFUNCTION(CallInEditor)
+	void DEBUG_FillInStructs();
 
 	FText GetReadableDialogueString() const;
 	FText GetSpeakerName() const;

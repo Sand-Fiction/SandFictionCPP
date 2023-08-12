df7b781e6ea6c1f76640419d4d4ae04c4d52d615
Moritz Peter <momomenion@gmail.com>
2023-08-12 18:00:46 +0200

    Recipe Popups, Automatic Recipe Learning after scanning

--- Source/SandFictionCPP/Components/SF_RecipeBookComponent.cpp
+++ Source/SandFictionCPP/Components/SF_RecipeBookComponent.cpp
@@ -16,11 +16,13 @@ USF_RecipeBookComponent::USF_RecipeBookComponent()
 void USF_RecipeBookComponent::AddRecipe(const FCraftingRecipe Recipe)
 {
 	KnownRecipes.AddUnique(Recipe);
+	OnRecipeAdded.Broadcast(Recipe);
 }
 
 void USF_RecipeBookComponent::RemoveRecipe(const FCraftingRecipe Recipe)
 {
 	KnownRecipes.Remove(Recipe);
+	OnRecipeRemoved.Broadcast(Recipe);
 }
 
 bool USF_RecipeBookComponent::HasRecipe(const FDataTableRowHandle RecipeData) const
@@ -34,14 +36,55 @@ bool USF_RecipeBookComponent::HasRecipe(const FDataTableRowHandle RecipeData) co
 	return false;
 }
 
+bool USF_RecipeBookComponent::HasRecipeItems(const FCraftingRecipe& Recipe) const
+{
+	bool HasItems = false;
+
+	if (GetOwner())
+	{
+		if (const auto Inventory = Cast<USF_InventoryComponent>(GetOwner()->FindComponentByClass<USF_InventoryComponent>()))
+		{
+			for (const auto InventoryData : Recipe.RecipeData)
+			{
+				HasItems = Inventory->HasItemInInventory(InventoryData);
+				if (!HasItems)
+				{
+					break;
+				}
+			}
+		}
+	}
+	return HasItems;
+}
+
+void USF_RecipeBookComponent::CheckRecipeUnlock(FInventoryData ItemData)
+{
+	const auto RowNames = RecipeDataTable->GetRowNames();
+	for (const auto RowName : RowNames)
+	{
+		if (const auto Recipe = RecipeDataTable->FindRow<FCraftingRecipe>(RowName, ""))
+		{
+			if (HasRecipeItems(*Recipe))
+			{
+				AddRecipe(*Recipe);
+			}
+		}
+	}
+}
+
 
 // Called when the game starts
 void USF_RecipeBookComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	if (GetOwner())
+	{
+		if (const auto Inventory = Cast<USF_InventoryComponent>(GetOwner()->FindComponentByClass<USF_InventoryComponent>()))
+		{
+			Inventory->OnItemAddedToInventory.AddDynamic(this, &USF_RecipeBookComponent::CheckRecipeUnlock);
+		}
+	}
 }
 
 
